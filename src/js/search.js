68af2057caea4cb806a6e4261b9f6b94acd5e7e5
Burak Alp <burakalp17@gmail.com>
2020-09-15 18:04:08 +0300

    last changes

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,88 @@
+var isHandheld = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var pageY;
+var windowHeight;
+
+var $parallaxImages;
+
+function commonScrollEvents() {
+  if (pageY > 200) $('header').addClass('scrolled');
+  else $('header').removeClass('scrolled');
+
+  $('main > section').each(function () {
+    var $section = $(this);
+    var $body = $('body');
+    var thisOT = $(this).position().top;
+    var thisOH = $(this).outerHeight();
+
+    if (thisOT <= pageY + 300 && thisOT + thisOH - 200 > pageY) {
+      $section.addClass('is-active');
+
+      if ($section.data('body-bg') != undefined) {
+        $body.attr('style', 'background-color:' + $section.data('body-bg'));
+        $body.addClass('no-effect-header');
+      }
+    } else {
+      $section.removeClass('is-active');
+
+      if ($section.data('body-bg') != undefined) {
+        $body.removeAttr('style');
+        $body.removeClass('no-effect-header');
+      }
+    }
+  });
+}
+
+$('.search').each(function () {
+  var $this = $(this);
+  $parallaxImages = $this.find('.parallax-images');
+
+  $('.menu-trigger-button').on('click', function () {
+    $('.header-menu-block').addClass('is-shown');
+  });
+
+  $('.header-menu-block').on('click', function (e) {
+    var target = $(e.target);
+
+    $('.header-menu-block').removeClass('is-shown');
+  });
+
+  $('[res-trigger]').on('click', function () {
+    var $closest = $(this).closest('.reservation-summary');
+
+    $closest.toggleClass('is-shown');
+  });
+
+  $(window).on({
+    load: () => {
+      scrollEvents();
+    },
+    scroll: () => {
+      scrollEvents();
+    },
+  });
+});
+
+function scrollEvents() {
+  pageY = window.pageYOffset;
+  windowHeight = window.innerHeight;
+
+  commonScrollEvents();
+  initParallaxImages();
+}
+
+function initParallaxImages() {
+  $parallaxImages.each(function () {
+    var $this = $(this);
+    var $thisSection = $this.closest('section');
+
+    if (!isHandheld && $thisSection.hasClass('is-active')) {
+      var transformVal = 'translateY(' + pageY / 2 + 'px)';
+
+      $this.attr('style', `transform:${transformVal}`);
+    }
+  });
+}
+
 $('.popover-trigger').on('click', function () {
   $(this).siblings().toggleClass('is-shown');
 });
@@ -25,4 +110,48 @@ $('.popovers').each(function () {
       $('.popovers').removeClass(cssClass);
     }
   });
+});
+
+$('.search-widget').each(function () {
+  $(this)
+    .find('.search-info')
+    .on('click', function () {
+      $(this).parent().addClass('is-shown');
+    });
+
+  $(this)
+    .find('.minimize-search')
+    .on('click', function () {
+      $(this).closest('.search-widget-block').removeClass('is-shown');
+    });
+});
+
+$('.rooms-block').each(function () {
+  var $roomItem = $(this).find('.rooms-item')[0].outerHTML;
+
+  for (let i = 0; i < 10; i++) {
+    $(this).append($roomItem);
+  }
+});
+
+$('.rooms-image-slider').each(function () {
+  var $this = $(this);
+
+  var config = {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+    containerModifierClass: 'rooms-image-slider--',
+    wrapperClass: 'rooms-image-wrapper',
+    slideClass: 'rooms-image-slide',
+    slideActiveClass: 'rooms-image-slide--active',
+    slideNextClass: 'rooms-image-slide--next',
+    slidePrevClass: 'rooms-image-slide--prev',
+    navigation: {
+      nextEl: $this.find('.next')[0],
+      prevEl: $this.find('.prev')[0],
+    },
+  };
+
+  var slider = new Swiper($this[0], config);
 });
